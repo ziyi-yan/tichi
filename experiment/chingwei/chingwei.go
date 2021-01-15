16ec9d73d6f2ee034c28153d473c3e0858c188ac
Ziyi Yan <ziyi.yan@foxmail.com>
2021-01-15 23:09:00 +0800

    wip: glue code

--- experiment/chingwei/chingwei.go
+++ experiment/chingwei/chingwei.go
@@ -20,7 +20,32 @@ func Reproducing(log *logrus.Entry, ghc githubClient) error {
 
 	for _, issue := range issues {
 		log.WithField("issue", issue).Info("Get issue success.")
+		
+		// TODO parse minimal reproduce step and version from issue
+		sql := "minimal reproduce step"
+		mysqlVersion := "8.0"
+		tidbVersion := "v5.0.0-rc"
+		
+		// TODO try send a SQL query to tidb with a specific version
+		tidbDSN, tidbCleanup, err := PrepareTiDB(tidbVersion)
+		mysqlDSN, mysqlCleanup, err := PrepareMySQL(mysqlVersion)
+	
+		// TODO: do reproduce by connecting to `dsn`
+		tidbOutput, tidbErr := Reproduce(tidbDSN, sql)
+		mysqlOutput, mysqlErr := Reproduce(mysqlDSN, sql)
+
+		// do cleanup
+		tidbCleanup()
+		mysqlCleanup()
 	}
 
+	
+
 	return nil
+}
+
+// PrepareTiDB start a tidb server with specific version.
+// It returns a function to destory and cleanup the related resources.
+func PrepareTiDB(version string) (string, func () {}, error) {
+
 }
